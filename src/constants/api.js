7e12564e80e81c636841253f3919e441cdbf1249
Sulfiya786 <sulfiya@unitdtechnologies.com>
2024-10-17 13:56:47 +0530

    firstcommit

---
+++ src/constants/api.js
@@ -0,0 +1,13 @@
+import axios from 'axios'
+
+const api = axios.create({
+//baseURL: 'http://43.228.126.245:4000',
+baseURL: 'https://arconsultancy.unitdtechnologies.com:2038'
+
+});
+
+
+
+
+
+export default api
